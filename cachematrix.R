443673208a4928f415d3a6e78a44c0d1676874d8
Salvador Di Forti <salvadordiforti180@gmail.com>
2024-08-15 19:46:05 -0400

    Primer Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Esta función crea un objeto "matriz" especial que puede almacenar en caché su inversa.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Esta función calcula la inversa de la "matriz" especial devuelta por makeCacheMatrix anteriormente. Si la inversa ya ha sido calculada (y la matriz no ha cambiado), entonces cacheSolve debe recuperar la inversa de la caché.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+# Crear una matriz simple
+m <- matrix(c(1, 2, 3, 4), 2, 2)
+
+# Crear un objeto especial de tipo matriz que puede almacenar su inversa
+cacheMatrix <- makeCacheMatrix(m)
+
+# Calcular la inversa usando cacheSolve
+cacheSolve(cacheMatrix)
+
+# Si llamas de nuevo a cacheSolve, debe devolver la inversa desde la caché
+cacheSolve(cacheMatrix)
